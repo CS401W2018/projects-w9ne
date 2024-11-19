5db44ac4cd00fbac06a26a3c0921ae54d29425fe
Jared Wong <jared119@gmail.com>
2024-11-19 03:37:52 -1000

    change to GET

--- projects/form/form.js
+++ projects/form/form.js
@@ -38,7 +38,7 @@ document.getElementById('jaredForms').addEventListener('submit', function (event
     }
 
     const xhr = new XMLHttpRequest();
-    xhr.open("POST", "submit.json", true);
+    xhr.open("GET", "submit.json", true);
     xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
 
     xhr.onreadystatechange = function () {
